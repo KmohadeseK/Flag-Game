f292ab31850fcd44e307ac69272a6b38da2117f0
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-09-14 02:00:25 +0300

    Revert "Merge branch 'main' of https://github.com/KmohadeseK/Flag-Game"
    This reverts commit 1f6753e48593ca968529ab68c84c2a74d89f9621, reversing
    changes made to 4b7e25a5f0faadbac22db4396bb263035b8903a1.

--- code/app.js
+++ code/app.js
@@ -1,50 +1,8 @@
-//..................↓ PLAYER BLUE 1 ↓....................
-
-var pl1 = document.querySelector(".p");
-var pl1TopPos = 0;
-var pl1LeftPos = 0;
-var sound = new Audio("Runing.mp3"); // Added audio file
-
-var isKeyPressed = false; // Saved Key Status Completed
-function moveImage(event) {
-    if (isKeyPressed) { // Checking the pressed key status
-        if (event.keyCode === 97) { // Left arrow key => (A)
-            pl1LeftPos -= 10;
-            pl1.style.left = pl1LeftPos + "px";
-        } else if (event.keyCode === 119) { // Top arrow key => (W)
-            pl1TopPos -= 10;
-            pl1.style.top = pl1TopPos + "px";
-        } else if (event.keyCode === 100) { // Right arrow key => (D)
-            pl1LeftPos += 10;
-            pl1.style.left = pl1LeftPos + "px";
-        } else if (event.keyCode === 115) { // Down arrow key => (S)
-            pl1TopPos += 10;
-            pl1.style.top = pl1TopPos + "px";
-        }
-        sound.play(); // Play audio
-    }
-}
-
-document.addEventListener("keydown", function (event) {
-    if (!isKeyPressed) {
-        isKeyPressed = true; // Set pressed key state to true
-        moveImage(event); // Call the moveImage function
-    }
-});
-
-document.addEventListener("keyup", function (event) {
-    isKeyPressed = false; // Set pressed key state to false
-    sound.pause(); // Stop playing audio
-});
-
-//..................↓ PLAYER RED 2 ↓....................
-
 var pl2 = document.querySelector(".pmm");
 var pl2TopPos = 0;
 var pl2LeftPos = 0;
-var sound = new Audio("Runing.mp3"); // Added audio file
+var sound = new Audio("Runing.mp3"); // Add audio file
 
-var isKeyPressed = false; // Saved Key Status Completed
 function moveImage(event) {
   switch (event.keyCode) {
     case 37: // left arrow key
