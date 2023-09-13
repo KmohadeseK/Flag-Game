1500c2014373c9d437557146fecb9ec7b6c42490
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-09-13 16:38:20 +0300

    fix not

--- code/app.js
+++ code/app.js
@@ -1,29 +1,50 @@
-var img = document.querySelector(".pmm");
-var topPos = 0;
-var leftPos = 0;
+var pl2 = document.querySelector(".pmm");
+var pl2TopPos = 0;
+var pl2LeftPos = 0;
 var sound = new Audio("Runing.mp3"); // Add audio file
 
 function moveImage(event) {
     switch (event.keyCode) {
         case 37: // left arrow key
-            leftPos -= 10;
-            img.style.left = leftPos + "px";
+        pl2LeftPos -= 10;
+            pl2.style.left = pl2LeftPos + "px";
+            
             break;
         case 38: // up arrow key
-            topPos -= 10;
-            img.style.top = topPos + "px";
+        pl2TopPos -= 10;
+            pl2.style.top = pl2TopPos + "px";
+
             break;
         case 39: // right arrow key
-            leftPos += 10;
-            img.style.left = leftPos + "px";
+        pl2LeftPos += 10;
+            pl2.style.left = pl2LeftPos + "px";
             break;
         case 40: // down arrow key
-            topPos += 10;
-            img.style.top = topPos + "px";
+        pl2TopPos += 10;
+            pl2.style.top = pl2TopPos + "px";
             break;
     }
     sound.play(); // Play Audio
 }
 
-document.addEventListener("keydown", moveImage); ener("keydown", moveImage);
+document.addEventListener("keydown", moveImage); 
 
+
+// حMoving the character 1
+// slect character 1
+let pl1 = document.querySelector(".p");
+// move the character varble
+let pl1TopPos = 0;
+let pl1LeftPos = 0;
+
+document.addEventListener("keydown", movePL1);
+
+function movePL1(e){
+    // go top
+    if(e.key == "w"){
+        pl1TopPos += 10;
+        pl1.getBoundingClientRect() 
+        pl1.x += pl1TopPos;
+        console.log(pl1.x);
+    }
+}
