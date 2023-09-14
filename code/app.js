32003fb309f6f7aaf07c8a44051d888a4feb193e
Mohandes85K <143397162+Mohandes85K@users.noreply.github.com>
2023-09-14 22:38:33 +0330

    Update app js by mohadese

--- code/app.js
+++ code/app.js
@@ -1,50 +1,95 @@
+//..................↓ PLAYER BLUE 1 ↓....................
+
+var pl1 = document.querySelector(".p");
+var pl1TopPos = 0;
+var pl1LeftPos = 0;
+var sound = new Audio("Runing.mp3"); // Added audio file
+
+var isKeyPressed = false; // Saved Key Status Completed
+function moveImage(event) {
+    if (isKeyPressed) { // Checking the pressed key status
+        if (event.keyCode === 97) { // Left arrow key => (A)
+            pl1LeftPos -= 10;
+            pl1.style.left = pl1LeftPos + "px";
+        } else if (event.keyCode === 119) { // Top arrow key => (W)
+            pl1TopPos -= 10;
+            pl1.style.top = pl1TopPos + "px";
+        } else if (event.keyCode === 100) { // Right arrow key => (D)
+            pl1LeftPos += 10;
+            pl1.style.left = pl1LeftPos + "px";
+        } else if (event.keyCode === 115) { // Down arrow key => (S)
+            pl1TopPos += 10;
+            pl1.style.top = pl1TopPos + "px";
+        }
+        sound.play(); // Play audio
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (!isKeyPressed) {
+        isKeyPressed = true; // Set pressed key state to true
+        moveImage(event); // Call the moveImage function
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    isKeyPressed = false; // Set pressed key state to false
+    sound.pause(); // Stop playing audio
+});
+
+//..................↓ PLAYER RED 2 ↓....................
+
 var pl2 = document.querySelector(".pmm");
 var pl2TopPos = 0;
 var pl2LeftPos = 0;
-var sound = new Audio("Runing.mp3"); // Add audio file
+var sound = new Audio("Runing.mp3"); // Added audio file
 
+var isKeyPressed = false; // Saved Key Status Completed
 function moveImage(event) {
-    switch (event.keyCode) {
-        case 37: // left arrow key
-        pl2LeftPos -= 10;
+    if (isKeyPressed) { // Checking the pressed key status
+        if (event.keyCode === 37) { // Left arrow key
+            pl2LeftPos -= 10;
             pl2.style.left = pl2LeftPos + "px";
-            
-            break;
-        case 38: // up arrow key
-        pl2TopPos -= 10;
+        } else if (event.keyCode === 38) { // Top arrow key
+            pl2TopPos -= 10;
             pl2.style.top = pl2TopPos + "px";
-
-            break;
-        case 39: // right arrow key
-        pl2LeftPos += 10;
+        } else if (event.keyCode === 39) { // Right arrow key
+            pl2LeftPos += 10;
             pl2.style.left = pl2LeftPos + "px";
-            break;
-        case 40: // down arrow key
-        pl2TopPos += 10;
+        } else if (event.keyCode === 40) { // Down arrow key
+            pl2TopPos += 10;
             pl2.style.top = pl2TopPos + "px";
-            break;
+        }
+        sound.play(); // Play audio
     }
-    sound.play(); // Play Audio
 }
 
-document.addEventListener("keydown", moveImage); 
+document.addEventListener("keydown", function (event) {
+    if (!isKeyPressed) {
+        isKeyPressed = true; // Set pressed key state to true
+        moveImage(event); // Call the moveImage function
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    isKeyPressed = false; // Set pressed key state to false
+    sound.pause(); // Stop playing audio
+});
 
 
-// حMoving the character 1
-// slect character 1
-let pl1 = document.querySelector(".p");
 // move the character varble
-let pl1TopPos = 0;
-let pl1LeftPos = 0;
+// let pl1TopPos = 0;
+// let pl1LeftPos = 0;
 
-document.addEventListener("keydown", movePL1);
+// document.addEventListener("keydown", movePL1);
 
-function movePL1(e){
-    // go top
-    if(e.key == "w"){
-        pl1TopPos += 10;
-        pl1.getBoundingClientRect() 
-        pl1.x += pl1TopPos;
-        console.log(pl1.x);
-    }
-}
+// function movePL1(e) {
+//     // go top
+//     if (e.key == "w") {
+//         pl1TopPos += 10;
+//         pl1.getBoundingClientRect()
+//         pl1.x += pl1TopPos;
+//         console.log(pl1.x);
+//     }
+// }
+
