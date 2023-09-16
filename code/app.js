57214a6b64a3ea5f4e446978911a0847e3f9a5c6
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-09-16 10:48:35 +0300

    add mane ha

--- code/app.js
+++ code/app.js
@@ -22,14 +22,37 @@ document.addEventListener("keydown", animationmovePL2);
 document.addEventListener("keyup", animationmovePL2Stop);
 // slect PL2
 let PL2 = document.querySelector(".PL2")
+let spaceAlki = 0 
 // function == move animation CK
 function animationmovePL2(e){
   // if == move CK ba key
+  console.log(e.keyCode );
+  if (e.keyCode == 32 & spaceAlki == 0){
+    alert("میخوای از صفحه بپره بیرون بیاد پیشت ؟")
+    spaceAlki +=1
+  }
+  else if(spaceAlki == 1){
+    alert("ببین بیا منظقی بهش فکر کنیم چطوری انتظار داری بپره ؟")
+    spaceAlki +=1
+  }
+  
+  else if(spaceAlki == 2){
+    alert("ما اینجا داریم زحمت میکشیم ...")
+    spaceAlki +=1
+  }
+  else if(spaceAlki == 3){
+    alert("اونی که اسپیس میزنو رو ...")
+    spaceAlki +=1
+  }
+
   if(e.keyCode == 65){
     PL2 =document.querySelector(".PL2").style.animationName ="move-left-run-PL2";
+    PL2 =document.querySelector(".PL2").style.backgroundImage =  "url(asist/img/PL2/normalleft.png)"
+
   }
   else if(e.keyCode == 68){
     PL2 =document.querySelector(".PL2").style.animationName ="move-right-run-PL2";
+    PL2 =document.querySelector(".PL2").style.backgroundImage =  "url(asist/img/PL2/normalRight.png)"
     
   }
 }
@@ -58,12 +81,15 @@ let PL1 = document.querySelector(".PL1")
 function animationmovePL1(e){
   // if == move CK ba key
   if(e.keyCode == "37"){
+    PL2 =document.querySelector(".PL1").style.backgroundImage =  "url(asist/img/PL1/normalLeft.png)"
     PL2 =document.querySelector(".PL1").style.animationName ="move-left-run-PL1";
 
   }
   else if(e.keyCode == "39"){
     PL2 =document.querySelector(".PL1").style.animationName ="move-right-run-PL1";
     
+    PL2 =document.querySelector(".PL1").style.backgroundImage =  "url(asist/img/PL1/normalRight.png)"
+
   }
 }
 
