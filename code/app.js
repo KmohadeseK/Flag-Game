ee89a992243a6d0560b65eb9f2cdb6f98abbc326
EngineerAmin6886 <127695649+EngineerAmin6886@users.noreply.github.com>
2023-09-17 04:42:07 +0330

    sound
    ejad seda bra chracter ha hengam davidan.

--- code/app.js
+++ code/app.js
@@ -19,7 +19,7 @@ function startTime() {
 // -----=animation PL2 move=-----
 
 // select safeh bazi
-let overlapGroup = document.querySelector('.overlap-group');
+let overlapGroup = document.querySelector(".overlap-group");
 
 document.addEventListener("keydown", animationmovePL2);
 document.addEventListener("keyup", animationmovePL2Stop);
@@ -32,87 +32,95 @@ let spaceAlki = 0;
 let PL2LeftMove = 0;
 let PL2TopMove = 0;
 
-function animationmovePL2(e) {
+// ejad sound
+let sound = new Audio("Runing.mp3");
 
+let iskeyPressed = false;
+
+function animationmovePL2(e) {
   // select mohgaeyat makani
   const road = overlapGroup.getBoundingClientRect();
   console.log(road);
 
-  // if == move CK ba key
-  if (e.keyCode == 65 && PL2LeftMove > 0) {
+  // key press
+  if (iskeyPressed) {
+    // if == move CK ba key
+    if (e.keyCode == 65 && PL2LeftMove > 0) {
+      // move PL2 left
+      PL2LeftMove -= 10;
+      PL2 = document.querySelector(".PL2").style.left = PL2LeftMove + "px";
 
-    // move PL2 left
-    PL2LeftMove -= 10;
-    PL2 = document.querySelector(".PL2").style.left = PL2LeftMove + "px";
+      // move animation
+      PL2 = document.querySelector(".PL2").style.animationName =
+        "move-left-run-PL2";
+      PL2 = document.querySelector(".PL2").style.backgroundImage =
+        "url(asist/img/PL2/normalleft.png)";
+    } else if (e.keyCode == 68 && PL2LeftMove < 1210) {
+      // move PL2 right
+      PL2LeftMove += 10;
+      PL2 = document.querySelector(".PL2").style.left = PL2LeftMove + "px";
 
-    // move animation
-    PL2 = document.querySelector(".PL2").style.animationName =
-      "move-left-run-PL2";
-    PL2 = document.querySelector(".PL2").style.backgroundImage =
-      "url(asist/img/PL2/normalleft.png)";
+      // move animation
+      PL2 = document.querySelector(".PL2").style.animationName =
+        "move-right-run-PL2";
+      PL2 = document.querySelector(".PL2").style.backgroundImage =
+        "url(asist/img/PL2/normalRight.png)";
+    } else if (e.keyCode == 87 && PL2TopMove > -57) {
+      // move PL2 top
+      PL2TopMove -= 10;
+      PL2 = document.querySelector(".PL2").style.top = PL2TopMove + "px";
 
-  } else if (e.keyCode == 68 && PL2LeftMove < 1210) {
+      // move animation
+    } else if (e.keyCode == 83 && PL2TopMove < 530) {
+      // move PL2 btoom
+      PL2TopMove += 10;
+      PL2 = document.querySelector(".PL2").style.top = PL2TopMove + "px";
+      // move animation
+    }
 
-    // move PL2 right
-    PL2LeftMove += 10;
-    PL2 = document.querySelector(".PL2").style.left = PL2LeftMove + "px";
-
-    // move animation
-    PL2 = document.querySelector(".PL2").style.animationName =
-      "move-right-run-PL2";
-    PL2 = document.querySelector(".PL2").style.backgroundImage =
-      "url(asist/img/PL2/normalRight.png)";
-
-  } else if (e.keyCode == 87 && PL2TopMove > -57){
-
-    // move PL2 top
-    PL2TopMove -= 10;
-    PL2 = document.querySelector(".PL2").style.top = PL2TopMove + "px";
-
-    // move animation
-  } else if (e.keyCode == 83 && PL2TopMove < 530) {
-
-    // move PL2 btoom
-    PL2TopMove += 10;
-    PL2 = document.querySelector(".PL2").style.top = PL2TopMove + "px";
-    // move animation
-  }
-
-  //space if
-  if ((e.keyCode == 32) & (spaceAlki == 0)) {
-    alert("میخوای از صفحه بپره بیرون بیاد پیشت ؟");
-    spaceAlki += 1;
-
-  } else if (spaceAlki == 1) {
-    alert("ببین بیا منظقی بهش فکر کنیم چطوری انتظار داری بپره ؟");
-    spaceAlki += 1;
-
-  } else if (spaceAlki == 2) {
-    alert("ما اینجا داریم زحمت میکشیم ...");
-    spaceAlki += 1;
-
-  } else if (spaceAlki == 3) {
-    alert("اونی که اسپیس میزنو رو ...");
-    spaceAlki += 1;
+    //space if
+    if ((e.keyCode == 32) & (spaceAlki == 0)) {
+      alert("میخوای از صفحه بپره بیرون بیاد پیشت ؟");
+      spaceAlki += 1;
+    } else if (spaceAlki == 1) {
+      alert("ببین بیا منظقی بهش فکر کنیم چطوری انتظار داری بپره ؟");
+      spaceAlki += 1;
+    } else if (spaceAlki == 2) {
+      alert("ما اینجا داریم زحمت میکشیم ...");
+      spaceAlki += 1;
+    } else if (spaceAlki == 3) {
+      alert("اونی که اسپیس میزنو رو ...");
+      spaceAlki += 1;
+    }
+    sound.play();
   }
 }
 
+// select key down bra sound
+document.addEventListener("keydown", function (event) {
+  if (!iskeyPressed) {
+    iskeyPressed = true;
+    animationmovePL2(event)
+  }
+});
+
+// select keyup bra sound
+document.addEventListener('keyup',function(){
+  iskeyPressed = false;
+  sound.pause();
+})
+
 // function == stop move animation CK
 function animationmovePL2Stop(e) {
-
   // if == stop move animation CK
   if (e.keyCode == 65) {
-
     PL2 = document.querySelector(".PL2").style.animationName = "none";
     PL2 = document.querySelector(".PL2").style.backgroundImage =
       "url(asist/img/PL2/normalleft.png)";
-
   } else if (e.keyCode == 68) {
-
     PL2 = document.querySelector(".PL2").style.animationName = "none";
     PL2 = document.querySelector(".PL2").style.backgroundImage =
       "url(asist/img/PL2/normalRight.png)";
-
   }
 }
 
@@ -129,10 +137,8 @@ let PL1LeftMove = 0;
 let PL1TopMove = 0;
 
 function animationmovePL1(e) {
-
   // if == move CK ba key
   if (e.keyCode == "37" && PL1LeftMove > -42) {
-
     // move CK left
     PL1LeftMove -= 10;
     PL1 = document.querySelector(".PL1").style.left = PL1LeftMove + "px";
@@ -142,9 +148,7 @@ function animationmovePL1(e) {
 
     PL1 = document.querySelector(".PL1").style.animationName =
       "move-left-run-PL1";
-
-  } else if (e.keyCode == "39"&& PL1LeftMove < 1149) {
-
+  } else if (e.keyCode == "39" && PL1LeftMove < 1149) {
     // move CK right
     PL1LeftMove += 10;
     PL1 = document.querySelector(".PL1").style.left = PL1LeftMove + "px";
@@ -154,40 +158,30 @@ function animationmovePL1(e) {
 
     PL1 = document.querySelector(".PL1").style.backgroundImage =
       "url(asist/img/PL1/normalRight.png)";
-
-  } else if (e.keyCode == "38"&& PL1TopMove > -50) {
-
+  } else if (e.keyCode == "38" && PL1TopMove > -50) {
     // move top
     PL1TopMove -= 10;
     PL1 = document.querySelector(".PL1").style.top = PL1TopMove + "px";
-
-  } else if (e.keyCode == "40"&& PL1TopMove < 530) {
-
+  } else if (e.keyCode == "40" && PL1TopMove < 530) {
     // move bttom
     PL1TopMove += 10;
     PL1 = document.querySelector(".PL1").style.top = PL1TopMove + "px";
-
   }
 }
 
 // function == stop move animation CK
 function animationmovePL1Stop(e) {
-
   // if == stop move animation CK
   if (e.keyCode == "37") {
-
     PL1 = document.querySelector(".PL1").style.animationName = "none";
 
     PL1 = document.querySelector(".PL1").style.backgroundImage =
       "url(asist/img/PL1/normalLeft.png)";
-
   } else if (e.keyCode == "39") {
-
     PL1 = document.querySelector(".PL1").style.animationName = "none";
 
     PL1 = document.querySelector(".PL1").style.backgroundImage =
       "url(asist/img/PL1/normalRight.png)";
-
   }
 }
 
